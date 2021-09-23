b42830318c4ee5ccbf00bea1947ded83aa462f9e
Triana Cerda <trianaRcerda@gmail.com>
2021-09-23 16:07:20 -0700

    added dummy data with 3 obj

---
+++ data/dummyData.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    id: 1,
+    messenger: 'triana',
+    funny: true,
+  },
+  {
+    id: 2,
+    messenger: 'alice',
+    funny: true,
+  },
+  {
+    id: 3,
+    messenger: 'simon',
+    funny: true,
+  },
+];
