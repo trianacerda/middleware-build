93ed1a5a4f92643c0605099318609eb867885381
Alice-Retz <alicegretz@gmail.com>
2021-09-24 17:16:41 -0500

    First pass tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,14 +3,69 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('demo routes', () => {
+// /GET--retrieve from third party api
+
+// -----DUMMY DATA-----
+
+// /POST--
+
+// /PUT--
+
+// /DELETE--
+
+describe('04_middleware-service-layer routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  afterAll(() => {
-    pool.end();
+  // afterAll(() => {
+  //   pool.end();
+  // });
+
+  it('creates a new entry', () => {
+    return request(app)
+      .post('/api/v1/orders')
+      .send({ ???? })
+      .then((res) => {
+        expect(res.body).toEqual({ ??? });
+      })
   });
 
-  it('/ GET ');
+  it('should return all entries', async () => {
+    const order = await Fmessage.insert({ ???? });
+    console.log('order in get all', order);
+    return request(app)
+      .get('/api/v1/orders')
+      .then((res) => {
+        expect(res.body).toEqual([order]);
+      });
+  });
+
+  it('should return an entry by id', async () => {
+    const order = await Fmessage.insert({ ???? });
+    return request(app)
+      .get(`/api/v1/orders/${order.id}`)
+      .then((res) => {
+        expect(res.body).toEqual(order);
+      });
+  });
+
+  it('should update an entry', async () => {
+    const order = await Fmessage.insert({ ???? });
+    return request(app)
+      .put(`/api/v1/orders/${order.id}`)
+      .send({ ??? })
+      .then((res) => {
+        expect(res.body).toEqual({ ?? });
+      });
+  });
+
+  it('should delete an entry', async () => {
+    const order = await Fmessage.insert({ ???? });
+    return request(app)
+      .delete(`/api/v1/orders/${order.id}`)
+      .then((res) => {
+        expect(res.body).toEqual({});
+      });
+  });
 });
