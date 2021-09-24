34791853f1c509659ebbd8e89bbdce1ced1ca5f0
Triana Cerda <trianaRcerda@gmail.com>
2021-09-24 16:10:34 -0700

    updated controller with .get .post .put .delete-- included .get/:id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Fmessage = require('../lib/models/FMessage');
 
 // /GET--retrieve from third party api
 
@@ -25,18 +26,14 @@ describe('04_middleware-service-layer routes', () => {
   it.only('creates a new entry', () => {
     return request(app)
       .post('/api/v1/orders')
-      .send({ messenger: 'aemilius',
-        funny: true, })
+      .send({ messenger: 'aemilius', funny: true })
       .then((res) => {
-        expect(res.body).toEqual({  id: 4,
-          messenger: 'aemilius',
-          funny: true,  });
+        expect(res.body).toEqual({ id: 4, messenger: 'aemilius', funny: true });
       });
   });
 
   it('should return all entries', async () => {
-    const order = await Fmessage.insert({ messenger: 'aemilius',
-      funny: true, });
+    const order = await Fmessage.insert({ messenger: 'aemilius', funny: true });
     return request(app)
       .get('/api/v1/orders')
       .then((res) => {
@@ -45,8 +42,7 @@ describe('04_middleware-service-layer routes', () => {
   });
 
   it('should return an entry by id', async () => {
-    const order = await Fmessage.insert({ messenger: 'aemilius',
-      funny: true, });
+    const order = await Fmessage.insert({ messenger: 'aemilius', funny: true });
     return request(app)
       .get(`/api/v1/orders/${order.id}`)
       .then((res) => {
@@ -55,21 +51,21 @@ describe('04_middleware-service-layer routes', () => {
   });
 
   it('should update an entry', async () => {
-    const order = await Fmessage.insert({ messenger: 'aemilius',
-      funny: true, });
+    const order = await Fmessage.insert({ messenger: 'aemilius', funny: true });
     return request(app)
       .put(`/api/v1/orders/${order.id}`)
-      .send({ funny: false, })
+      .send({ funny: false })
       .then((res) => {
-        expect(res.body).toEqual({ id: 4,
+        expect(res.body).toEqual({
+          id: 4,
           messenger: 'aemilius',
-          funny: false, });
+          funny: false,
+        });
       });
   });
 
   it('should delete an entry', async () => {
-    const order = await Fmessage.insert({ messenger: 'aemilius',
-      funny: true, });
+    const order = await Fmessage.insert({ messenger: 'aemilius', funny: true });
     return request(app)
       .delete(`/api/v1/orders/${order.id}`)
       .then((res) => {
